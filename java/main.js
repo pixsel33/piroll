dfdcc7b60ffba13db1cfec786404a63eead1be29
pixsel33 <>
2023-01-30 22:15:37 +0300

    first commit

---
+++ java/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+  $('.slider').slick({
+ 
+  });
+});
+
+var line = document.getElementById('progress_line');
+window.addEventListener('scroll', progressBar);
+       
+function progressBar(e) {
+   var windowScroll = document.body.scrollTop || 
+   document.documentElement.scrollTop;
+   var windowHeight = document.documentElement.scrollHeight - 
+   document.documentElement.clientHeight; 
+   var width_progress_line = windowScroll / windowHeight * 100;
+   line.style.width = width_progress_line + '%';
+}
